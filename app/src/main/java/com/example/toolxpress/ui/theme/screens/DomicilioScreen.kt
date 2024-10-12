b982d3ede217cc731a2fba88e1aa02bf2b21ba9b
DaianNieves <daiannieves10@gmail.com>
2024-10-12 13:30:56 -0600

    Login Redesign

--- app/src/main/java/com/example/toolxpress/ui/theme/screens/DomicilioScreen.kt
+++ app/src/main/java/com/example/toolxpress/ui/theme/screens/DomicilioScreen.kt
@@ -26,6 +26,7 @@ import com.example.toolxpress.ui.theme.Orange
 @Composable
 fun DomicilioScreen(navController: NavController) {
 
+    //Declaración de Variables
     var name by remember { mutableStateOf("") }
     var postalCode by remember { mutableStateOf("") }
     var state by remember { mutableStateOf("") }
@@ -37,193 +38,214 @@ fun DomicilioScreen(navController: NavController) {
     var betweenStreet2 by remember { mutableStateOf("") }
     var isHome by remember { mutableStateOf(true) }
     var contactNumber by remember { mutableStateOf("") }
-
-    // Scroll state
     val scrollState = rememberScrollState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(scrollState) // Habilitar scroll vertical
-            .padding(16.dp)
-    ) {
 
-        Spacer(modifier = Modifier.height(100.dp))
-        // Header
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Barra Superior
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Orange)
-                .padding(16.dp)
+                .padding(horizontal = 0.dp)
+                .statusBarsPadding()
         ) {
-            Icon(
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = "Back",
-                tint = Color.Black,
-                modifier = Modifier.clickable {
-                    navController.popBackStack() // Acción para regresar a la pantalla anterior
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Orange)
+                    .padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back",
+                    tint = Color.White,
+                    modifier = Modifier.clickable {
+                        navController.popBackStack() // Acción para regresar a la pantalla anterior
+                    }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Agrega un domicilio",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+        }
+
+
+        // Contenido Principal
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState) // Habilitar scroll vertical
+                    .padding(16.dp)
+            ) {
+
+                // Nombre y apellido
+                TextFieldWithLabel(
+                    label = "Nombre y apellido",
+                    value = name,
+                    onValueChange = { name = it },
+                    placeholder = "Tal cual figure en el INE o IFE."
+                )
+
+                // Espaciado uniforme
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Código postal (Validar que solo se ingresen números)
+                TextFieldWithLabel(
+                    label = "Código postal",
+                    value = postalCode,
+                    onValueChange = {
+                        if (it.all { char -> char.isDigit() }) postalCode =
+                            it // Validar que solo se ingresen números
+                    },
+                    placeholder = "Ingresa tu código postal",
+                    keyboardType = KeyboardType.Number
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Estado
+                TextFieldWithLabel(
+                    label = "Estado",
+                    value = state,
+                    onValueChange = { state = it },
+                    placeholder = "Ingresa tu estado"
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Municipio/Alcaldía
+                TextFieldWithLabel(
+                    label = "Municipio/Alcaldía",
+                    value = city,
+                    onValueChange = { city = it },
+                    placeholder = "Ingresa tu municipio o alcaldía"
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Colonia
+                TextFieldWithLabel(
+                    label = "Colonia",
+                    value = colony,
+                    onValueChange = { colony = it },
+                    placeholder = "Ingresa tu colonia"
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Calle
+                TextFieldWithLabel(
+                    label = "Calle",
+                    value = street,
+                    onValueChange = { street = it },
+                    placeholder = "Escribe solo el nombre de la calle o avenida."
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Número (Validar que solo se ingresen números)
+                TextFieldWithLabel(
+                    label = "Número",
+                    value = houseNumber,
+                    onValueChange = {
+                        if (it.all { char -> char.isDigit() }) houseNumber =
+                            it // Validar que solo se ingresen números
+                    },
+                    placeholder = "Ingresa el número"
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Entre qué calles está
+                TextFieldWithLabel(
+                    label = "¿Entre qué calles está?",
+                    value = betweenStreet1,
+                    onValueChange = { betweenStreet1 = it },
+                    placeholder = "Calle 1"
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                TextFieldWithLabel(
+                    label = "",
+                    value = betweenStreet2,
+                    onValueChange = { betweenStreet2 = it },
+                    placeholder = "Calle 2"
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // ¿Es tu trabajo o tu casa?
+                Text(
+                    text = "¿Es tu trabajo o tu casa?",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 8.dp)
+                ) {
+                    RadioButton(
+                        selected = isHome,
+                        onClick = { isHome = true }
+                    )
+                    Text(text = "Casa", modifier = Modifier.padding(start = 8.dp))
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    RadioButton(
+                        selected = !isHome,
+                        onClick = { isHome = false }
+                    )
+                    Text(text = "Trabajo", modifier = Modifier.padding(start = 8.dp))
                 }
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = "Agrega un domicilio",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-        }
 
-        Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
 
-        // Nombre y apellido
-        TextFieldWithLabel(
-            label = "Nombre y apellido",
-            value = name,
-            onValueChange = { name = it },
-            placeholder = "Tal cual figure en el INE o IFE."
-        )
+                // Teléfono de contacto (Validar que solo se ingresen números)
+                TextFieldWithLabel(
+                    label = "Teléfono de contacto",
+                    value = contactNumber,
+                    onValueChange = {
+                        if (it.all { char -> char.isDigit() }) contactNumber =
+                            it // Validar que solo se ingresen números
+                    },
+                    placeholder = "Ingresa tu teléfono de contacto",
+                    keyboardType = KeyboardType.Phone
+                )
 
-        Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(24.dp)) // Espaciado antes del botón
 
-        // Código postal (Validar que solo se ingresen números)
-        TextFieldWithLabel(
-            label = "Código postal",
-            value = postalCode,
-            onValueChange = {
-                if (it.all { char -> char.isDigit() }) postalCode = it // Validar que solo se ingresen números
-            },
-            placeholder = "Ingresa tu código postal",
-            keyboardType = KeyboardType.Number
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Estado
-        TextFieldWithLabel(
-            label = "Estado",
-            value = state,
-            onValueChange = { state = it },
-            placeholder = "Ingresa tu estado"
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Municipio/Alcaldía
-        TextFieldWithLabel(
-            label = "Municipio/Alcaldía",
-            value = city,
-            onValueChange = { city = it },
-            placeholder = "Ingresa tu municipio o alcaldía"
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Colonia
-        TextFieldWithLabel(
-            label = "Colonia",
-            value = colony,
-            onValueChange = { colony = it },
-            placeholder = "Ingresa tu colonia"
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Calle
-        TextFieldWithLabel(
-            label = "Calle",
-            value = street,
-            onValueChange = { street = it },
-            placeholder = "Escribe solo el nombre de la calle o avenida."
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Número (Validar que solo se ingresen números)
-        TextFieldWithLabel(
-            label = "Número",
-            value = houseNumber,
-            onValueChange = {
-                if (it.all { char -> char.isDigit() }) houseNumber = it // Validar que solo se ingresen números
-            },
-            placeholder = "Ingresa el número"
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Entre qué calles está
-        TextFieldWithLabel(
-            label = "¿Entre qué calles está?",
-            value = betweenStreet1,
-            onValueChange = { betweenStreet1 = it },
-            placeholder = "Calle 1"
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextFieldWithLabel(
-            label = "",
-            value = betweenStreet2,
-            onValueChange = { betweenStreet2 = it },
-            placeholder = "Calle 2"
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // ¿Es tu trabajo o tu casa?
-        Text(
-            text = "¿Es tu trabajo o tu casa?",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            RadioButton(
-                selected = isHome,
-                onClick = { isHome = true }
-            )
-            Text(text = "Casa", modifier = Modifier.padding(start = 8.dp))
-
-            Spacer(modifier = Modifier.width(16.dp))
-
-            RadioButton(
-                selected = !isHome,
-                onClick = { isHome = false }
-            )
-            Text(text = "Trabajo", modifier = Modifier.padding(start = 8.dp))
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Teléfono de contacto (Validar que solo se ingresen números)
-        TextFieldWithLabel(
-            label = "Teléfono de contacto",
-            value = contactNumber,
-            onValueChange = {
-                if (it.all { char -> char.isDigit() }) contactNumber = it // Validar que solo se ingresen números
-            },
-            placeholder = "Ingresa tu teléfono de contacto",
-            keyboardType = KeyboardType.Phone
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        // Botón de guardar
-        Button(
-            onClick = {
-                navController.navigate("EnvioScreen")
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = Orange)
-        ) {
-            Text(
-                text = "Guardar domicilio",
-                color = Color.White,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
-            )
+                // Botón de guardar
+                Button(
+                    onClick = {
+                        navController.navigate("EnvioScreen")
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = Orange)
+                ) {
+                    Text(
+                        text = "Guardar domicilio",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
         }
     }
 }
