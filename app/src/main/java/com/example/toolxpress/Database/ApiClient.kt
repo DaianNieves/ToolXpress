aa543fde1cea622ea67b3609b37388e122ac53c5
DavidRangel2405 <21151065@aguascalientes.tecnm.mx>
2024-12-08 23:51:04 -0600

    Feat: sparkles: (ShoppingCartScreen) Implementation to ApiClient with server

--- app/src/main/java/com/example/toolxpress/Database/ApiClient.kt
+++ app/src/main/java/com/example/toolxpress/Database/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     private var retrofit: Retrofit? = null
-    private const val BASE_URL = "http://10.0.2.2:5000/"
+    private const val BASE_URL = "https://backend-joq6.onrender.com/"
 
     val client: Retrofit?
         get() {
